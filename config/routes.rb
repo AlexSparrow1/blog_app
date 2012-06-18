1effb0938dc2a4cd0372c07de61afbe34f9cfc59
Alex Sparrow <alexsparrow1@gmail.com>
2012-06-17 20:56:04 -0400

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 BlogApp::Application.routes.draw do
-  get "users/new"
-
   root to: "static_pages#home"
 
-  match "/signup", to: "users#new"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/help', to: "static_pages#help"
-  match '/about', to: "static_pages#about"
-  match '/contact', to: "static_pages#contact"
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
